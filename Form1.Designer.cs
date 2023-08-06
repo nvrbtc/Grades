f55dfbfcb02ea3e31df85cf1fb7fcc864edadd55
nvrbtc <141575425+nvrbtc@users.noreply.github.com>
2023-08-06 19:20:30 +0200

    Add files via upload
    Raw version

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace PROG_PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btEng = new System.Windows.Forms.Button();
+            this.btPl = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btCheck = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.dvgGradeList = new System.Windows.Forms.DataGridView();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.btDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgGradeList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btEng
+            // 
+            this.btEng.Location = new System.Drawing.Point(12, 12);
+            this.btEng.Name = "btEng";
+            this.btEng.Size = new System.Drawing.Size(51, 45);
+            this.btEng.TabIndex = 0;
+            this.btEng.Text = "ENG";
+            this.btEng.UseVisualStyleBackColor = true;
+            this.btEng.Click += new System.EventHandler(this.btEng_Click);
+            // 
+            // btPl
+            // 
+            this.btPl.Location = new System.Drawing.Point(69, 12);
+            this.btPl.Name = "btPl";
+            this.btPl.Size = new System.Drawing.Size(51, 45);
+            this.btPl.TabIndex = 1;
+            this.btPl.Text = "PL";
+            this.btPl.UseVisualStyleBackColor = true;
+            this.btPl.Click += new System.EventHandler(this.btPl_Click);
+            // 
+            // btAdd
+            // 
+            this.btAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btAdd.Location = new System.Drawing.Point(29, 358);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(114, 44);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btCheck
+            // 
+            this.btCheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btCheck.Location = new System.Drawing.Point(230, 358);
+            this.btCheck.Name = "btCheck";
+            this.btCheck.Size = new System.Drawing.Size(114, 44);
+            this.btCheck.TabIndex = 3;
+            this.btCheck.Text = "Update";
+            this.btCheck.UseVisualStyleBackColor = true;
+            this.btCheck.Click += new System.EventHandler(this.btCheck_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(443, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "+1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(24, 186);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 16);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            this.lblName.Click += new System.EventHandler(this.lblName_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(119, 180);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(24, 113);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(39, 16);
+            this.lblID.TabIndex = 7;
+            this.lblID.Text = "Index";
+            this.lblID.Click += new System.EventHandler(this.lblID_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(119, 107);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 8;
+            // 
+            // dvgGradeList
+            // 
+            this.dvgGradeList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dvgGradeList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgGradeList.Location = new System.Drawing.Point(246, 55);
+            this.dvgGradeList.Name = "dvgGradeList";
+            this.dvgGradeList.RowHeadersWidth = 51;
+            this.dvgGradeList.RowTemplate.Height = 24;
+            this.dvgGradeList.Size = new System.Drawing.Size(542, 275);
+            this.dvgGradeList.TabIndex = 9;
+            this.dvgGradeList.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dvgGrade_RowHeaderMouseClick);
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(26, 263);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(45, 16);
+            this.lblGrade.TabIndex = 12;
+            this.lblGrade.Text = "Grade";
+            this.lblGrade.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(119, 257);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(100, 22);
+            this.txtGrade.TabIndex = 13;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btDelete.Location = new System.Drawing.Point(645, 358);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(114, 44);
+            this.btDelete.TabIndex = 14;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.dvgGradeList);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btCheck);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.btPl);
+            this.Controls.Add(this.btEng);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgGradeList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btEng;
+        private System.Windows.Forms.Button btPl;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btCheck;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.DataGridView dvgGradeList;
+        private System.Windows.Forms.Label lblGrade;
+        public System.Windows.Forms.TextBox txtName;
+        public System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Button btDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using PROG_PROJECT.ProjectClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROG_PROJECT
+{
+    public partial class Form1 : Form
+    {
+        enum Lang{
+            ENG, 
+            PL
+        }
+        public string[] nameTxt = new string[2] { "Name", "Imie" };
+        public string[] gradeTxt = new string[2] { "Grade", "Ocena" };
+        public string[] addBt = new string[2] { "Add", "Dodac" };
+        public string[] updateBt = new string[2] { "Update", "Odswiezyc" };
+        public string[] deleteBt = new string[2] { "Delete", "Ususnac" };
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = SystemColors.Window;
+            btAdd.MouseEnter += OnMouseEnterBt; 
+            btAdd.MouseLeave += OnMouseLeaveBt;
+            btCheck.MouseEnter += OnMouseEnterBt;
+            btCheck.MouseLeave += OnMouseLeaveBt;
+            button1.MouseEnter += OnMouseEnterBt;
+            button1.MouseLeave += OnMouseLeaveBt;
+            btDelete.MouseEnter += OnMouseEnterBt1;
+            btDelete.MouseLeave += OnMouseLeaveBt1;
+            btPl.MouseEnter += btLang_Hower;
+            btEng.MouseEnter += btLang_Hower;
+            btPl.MouseLeave += btLang_HowerLeave;
+            btEng.MouseLeave += btLang_HowerLeave;
+        }
+
+       
+
+        private void OnMouseEnterBt(object sender, EventArgs e)
+        {
+            Button tb = sender as Button;
+            tb.BackColor = Color.Green; // or Color.Red or whatever you want
+            tb.ForeColor = Color.Blue; 
+        }
+        private void OnMouseLeaveBt(object sender, EventArgs e)
+        {
+            Button tb = sender as Button;
+            tb.BackColor = SystemColors.Control; // or Color.Red or whatever you want
+            tb.ForeColor = Color.Black;
+        }
+        private void OnMouseEnterBt1(object sender, EventArgs e)
+        {
+            Button tb = sender as Button;
+            tb.BackColor = Color.Red; // or Color.Red or whatever you want
+            tb.ForeColor = Color.White;
+        }
+        private void OnMouseLeaveBt1(object sender, EventArgs e)
+        {
+            Button tb = sender as Button;
+            tb.BackColor = SystemColors.Control; // or Color.Red or whatever you want
+            tb.ForeColor = Color.Black;
+        }
+
+        private void OnMouseLeaveButton1(object sender, EventArgs e)
+        {
+            button1.BackColor = SystemColors.ButtonFace;
+        }
+        grade c = new grade(); 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            c.Name = txtName.Text; 
+            c.Grade = Convert.ToInt32(txtGrade.Text);
+            bool succes = c.Insert(c);
+            if (succes == true )
+            {
+                MessageBox.Show("Insert Succeded.");
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Try again.");
+            }
+            DataTable dt = c.Select();
+            //dgvContactList.DataSource = dt;
+            dvgGradeList.DataSource = dt;
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCheck_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(txtID.Text).Equals(0) || txtName.Text.Equals(null) || Convert.ToInt32(txtGrade.Text).Equals(0))
+            {
+                MessageBox.Show("Data not set.");
+                return;
+            }
+            c.Id = int.Parse(txtID.Text);
+            c.Name = txtName.Text;
+            c.Grade = Convert.ToInt32(txtGrade.Text);
+            if (c.Grade > 100)
+            {
+                MessageBox.Show("Grade is over 100 pts ( maximum ).");
+                return;
+            }
+            bool succes = c.Update(c);
+            if ( succes == true )
+            {
+                MessageBox.Show("Update Succeded.");
+                Clear(); 
+            }
+            else
+            {
+                MessageBox.Show("Update Failed."); 
+
+            }
+            DataTable dt = c.Select(); 
+            dvgGradeList.DataSource = dt;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = c.Select();
+            dvgGradeList.DataSource = dt; 
+
+        }
+        public void Clear()
+        {
+            txtGrade.Text = "";
+            txtName.Text = "";
+            txtID.Text = "";
+
+        }
+        private void dvgGrade_RowHeaderMouseClick ( object sender , DataGridViewCellMouseEventArgs e)
+        {
+            int rowIndex = e.RowIndex; 
+            txtGrade.Text = dvgGradeList.Rows[rowIndex].Cells[2].Value.ToString();
+            txtName.Text = dvgGradeList.Rows[rowIndex].Cells[1].Value.ToString();
+            txtID.Text = dvgGradeList.Rows[rowIndex].Cells[0].Value.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if ( txtName.Text.Equals(null) || Convert.ToInt32(txtGrade.Text).Equals(0))
+            {
+                MessageBox.Show("Data not set.");
+                return;
+            }
+            c.Id = int.Parse(txtID.Text);
+            c.Name = txtName.Text;
+            c.Grade = Convert.ToInt32(txtGrade.Text) + 1;
+
+            if ( c.Grade > 100)
+            {
+                MessageBox.Show("Grade is over 100 pts ( maximum ).");
+                return; 
+            }
+            bool succes = c.Update(c);
+            if (succes == true)
+            {
+                MessageBox.Show("+ 1 punkt to " + c.Name + ".");
+                
+            }
+            else
+            {
+                MessageBox.Show("Update Failed.");
+
+            }
+            DataTable dt = c.Select();
+            dvgGradeList.DataSource = dt;
+           
+    }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(txtID.Text).Equals(0) || txtName.Text.Equals(null) || Convert.ToInt32(txtGrade.Text).Equals(0))
+            {
+                MessageBox.Show("Data not set.");
+                return;
+            }
+            c.Id = int.Parse(txtID.Text);
+            bool succes = c.Delete(c);
+            if ( succes == true)
+            {
+                MessageBox.Show("Deleted succesfully.");
+                Clear();
+                
+            }
+            else
+            {
+                MessageBox.Show("Went wrong."); 
+            }
+            DataTable dt = c.Select();
+
+            
+            dvgGradeList.DataSource = dt;
+
+        }
+
+        private void lblID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btEng_Click(object sender, EventArgs e)
+        {
+            lblGrade.Text = gradeTxt[Convert.ToInt32(Lang.ENG)];
+            lblName.Text = nameTxt[Convert.ToInt32(Lang.ENG)];
+            btAdd.Text = addBt[Convert.ToInt32(Lang.ENG)];
+            btCheck.Text = updateBt[Convert.ToInt32(Lang.ENG)];
+            btDelete.Text = deleteBt[Convert.ToInt32(Lang.ENG)];
+        }
+
+        private void btPl_Click(object sender, EventArgs e)
+        {
+            lblGrade.Text = gradeTxt[Convert.ToInt32(Lang.PL)];
+            lblName.Text = nameTxt[Convert.ToInt32(Lang.PL)];
+            btAdd.Text = addBt[Convert.ToInt32(Lang.PL)];
+            btCheck.Text = updateBt[Convert.ToInt32(Lang.PL)];
+            btDelete.Text = deleteBt[Convert.ToInt32(Lang.PL)];
+        }
+        private void btLang_Hower ( object sender, EventArgs e)
+        {
+            Button temp = sender as Button;
+            temp.BackColor = Color.Black;
+            temp.ForeColor = Color.White;
+        }
+
+        private void btLang_HowerLeave(object sender, EventArgs e)
+        {
+            Button temp = sender as Button;
+            temp.BackColor = SystemColors.Control;
+            temp.ForeColor = Color.Black;
+        }
+
+
+
+    }
+}
